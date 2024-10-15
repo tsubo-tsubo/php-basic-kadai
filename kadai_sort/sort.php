57e613c861442079962a78a8ce1db66e7071e46a
tsubo-tsubo <165967215+tsubo-tsubo@users.noreply.github.com>
2024-10-15 15:01:22 +0900

    Add files via upload

---
+++ kadai_sort/sort.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+
+<body>
+    <p>
+    <?php
+
+$nums = [15, 4, 18, 23, 10];
+
+function sort_2way($nums, $order) {
+    // $orderがTRUEなら昇順に、FALSEなら降順にソート
+    if ($order) {
+        echo "昇順にソートします。<br>";
+        sort($nums);  // 昇順にソート
+    } else {
+        echo "降順にソートします。<br>";
+        rsort($nums);  // 降順にソート
+    }
+
+    // ソート結果を表示
+    foreach ($nums as $value) {
+        echo $value . "<br>";
+    }
+}
+
+// 昇順ソート
+sort_2way($nums, true);
+
+// 降順ソート
+sort_2way($nums, false);
+
+?>
+</p>
+
+<p>
+    <?php
+
+$array = [15, 4, 18, 23, 10];
+
+function sort_2way($array, $order) {
+    // $orderがTRUEなら昇順に、FALSEなら降順にソート
+    if ($order) {
+        echo "昇順にソートします。<br>";
+        sort($array);  // 昇順にソート
+    } else {
+        echo "降順にソートします。<br>";
+        rsort($array);  // 降順にソート
+    }
+
+    // ソート結果を表示
+    foreach ($array as $value) {
+        echo $value . "<br>";
+    }
+}
+
+// 昇順ソート
+sort_2way($array, true);
+
+// 降順ソート
+sort_2way($array, false);
+
+?>
+</p>
+
+
+</body>
+
+</html>
+
